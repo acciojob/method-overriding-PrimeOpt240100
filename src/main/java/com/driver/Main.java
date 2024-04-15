122bb95e514c4395161c0af2b37d6769ff6deed0
Manish Sharma <msgurugram240100@gmail.com>
2024-04-15 21:07:47 +0530

    Assignment Submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+
+      // Task 3
+      B obj = new B();
+
+      // Task 5
+      obj.meth();
+  }
+}
+// Task 1
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+// Task 2
+class B extends A{
+    public B(){
+        // Task 3
+        super.meth();
+    }
+    // Task 4
+
+    @Override
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
 }
